0177f36008c3c623267946e6e8be2de449eef71d
khanamir <amir.khan.offenbach.main@gmail.com>
2020-05-13 07:19:56 +0200

    *Authenticationfunctions.py added with Example on how to use the functions *Read all defects.py added with example on how to use Authenticationfunctions.py

---
+++ Basics/Authentication/Authenticationfunctions.py
@@ -0,0 +1,44 @@
+# Required imports
+import json
+import requests
+
+
+def sign_in(url, client_id, client_secret):
+    resource = 'authentication/sign_in'
+    payload = {"client_id": client_id, "client_secret": client_secret}
+    ContentType = {'Content-Type': 'application/json', 'ALM_OCTANE_TECH_PREVIEW': 'true'}
+
+    # Send request and receive response (REST API)
+    resp = requests.post(url + '/' + resource,
+                         data=json.dumps(payload),
+                         headers=ContentType)
+
+    # SAVE Cookie and use in all subsequent requests
+    cookie = resp.cookies
+    print('Login to ALM Octane response code: ' + str(resp.status_code))
+    return resp.status_code, cookie
+
+
+def sign_out(url, cookie):
+    resource = 'authentication/sign_out'
+    ContentType = {'Content-Type': 'application/json', 'ALM_OCTANE_TECH_PREVIEW': 'true'}
+
+    # Send request to sign out - important to sign out from the session, provide the cookie received at sign in
+    resp = requests.post(url + '/' + resource,
+                         headers=ContentType,
+                         cookies=cookie)
+
+    print('Logout to ALM Octane response code: ' + str(resp.status_code))
+    return resp.status_code
+
+
+# ------ EXAMPLE How to use the sign_in and sign_out function
+url = "https://almoctane-eur.saas.microfocus.com"
+client_id = "Python_1wqe213qwe35tt34o97oz4o"
+client_secret = "(1236123sad324324ZQ"
+
+response_code, cookie = sign_in(url, client_id, client_secret)
+print("Call to sign_in to ALM Octane response code: " + str(response_code))
+
+response_code = sign_out(url,cookie)
+print("Call to sign_out to ALM Octane response code: " + str(response_code))
---
+++ Basics/defects/Read all defects.py
@@ -0,0 +1,47 @@
+# Using the basic authentication functions and define the whole package as Auth
+import requests
+
+import Basics.Authentication.Authenticationfunctions as Auth
+
+# ------ EXAMPLE How to use the sign_in and sign_out function
+url = "https://almoctane-eur.saas.microfocus.com"
+client_id = "Python_1wqe213qwe35tt34o97oz4o"
+client_secret = "(1236123sad324324ZQ"
+ContentType = {'Content-Type': 'application/json', 'ALM_OCTANE_TECH_PREVIEW': 'true'}
+
+# Sign_in to ALM Octane and save cookie
+response_code, cookie = Auth.sign_in(url, client_id,client_secret)
+
+#Define ALM Octane shared_space and workspace
+shared_space = '146008'
+workspace = '4001'
+
+#Read Defects using the cookie from the #Sign_in function
+resource='defects'
+
+#Storing the response into defects variable
+defects = requests.get(url+'/api/shared_spaces/' + shared_space + '/workspaces/'+ workspace +'/'+resource,
+                    headers=ContentType,
+                     cookies=cookie)
+
+print('Getting Defects Status: ' + str(defects.status_code))
+
+#Storing the JSON in response into defect_data
+defects_data = defects.json()
+
+#Get the attribute 'total_count'
+total_count = defects_data['total_count']
+
+#Get the attribute 'data' in to defect_list
+defect_list = defects_data['data']
+
+print('Total Defects: ' + str(total_count))
+
+#iterate through all defects within defect_list
+for defect in defect_list:
+    #Printing defect fields
+    print('Defect Summary: ' + defect['name'] + ', ID: ' + defect['id'] + ', Phase: ' + defect['phase']['id'])
+
+
+# Sign_out from ALM Octane
+response_code = Auth.sign_out(url, cookie)
